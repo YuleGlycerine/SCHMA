6ea76ddaca759824d7454d569b1495c012d103f8
YuleGlycerine <figurefig13@gmail.com>
2024-07-23 14:52:43 +0900

    initial commit

---
+++ SCHMA/SCHMAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SCHMAApp.swift
+//  SCHMA
+//
+//  Created by Yule on 7/23/24.
+//
+
+import SwiftUI
+
+@main
+struct SCHMAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
